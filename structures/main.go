a413de7c1e51298cf2ed3c6cbb195f5ac90fd56a
Wakisa Mwabulambo <wakielambo@gmail.com>
2024-12-19 11:10:36 +0200

    After accessing struct fields through a pointer

--- structures/main.go
+++ structures/main.go
@@ -12,7 +12,7 @@ type subscriber struct {
 
 func main() {
 	subscriber1 := defaultSubscriber("Aman Singh")
-	subscriber1.rate = 4.99 // use custom rate
+	applyDiscount(subscriber1)
 	printInfo(subscriber1)
 
 	subscriber2 := defaultSubscriber("Beth Ryan")
@@ -20,16 +20,20 @@ func main() {
 }
 
 // returns a subscriber value
-func defaultSubscriber(name string) subscriber {
+func defaultSubscriber(name string) *subscriber {
 	var s subscriber
 	s.name = name
 	s.rate = 5.99
 	s.active = true
-	return s
+	return &s
 }
 
-func printInfo(s subscriber) {
+func printInfo(s *subscriber) {
 	fmt.Println("Name:", s.name)
 	fmt.Println("Monthly rate:", s.rate)
 	fmt.Println("Active?", s.active)
+}
+
+func applyDiscount(s *subscriber) {
+	s.rate = 4.99
 }
