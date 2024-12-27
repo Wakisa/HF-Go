2be73804d9d1bddafe72479c9c767dd7f759dcf2
Wakisa Mwabulambo <wakielambo@gmail.com>
2024-12-27 11:33:31 +0200

    After using annonymous struct field

--- structures/main.go
+++ structures/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	address := magazine.Address{Street: "123 Oak St", City: "Omala", State: "NE", PostalCode: "68111"}
 	subscriber := magazine.Subscriber{Name: "Aman Singh"}
-	subscriber.HomeAddress = address
-	fmt.Println(subscriber.HomeAddress)
+	subscriber.Address = address //calling annonymous struct field in magazine package
+	fmt.Println(subscriber.Address)
 
 }
