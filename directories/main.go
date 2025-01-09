988e37af8de0a743eedf35b5001c920aac1f2a17
Wakisa Mwabulambo <wakielambo@gmail.com>
2025-01-09 10:01:20 +0200

    Error handling using panic

--- directories/main.go
+++ directories/main.go
@@ -2,35 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 )
 
-func scanDirectory(path string) error {
+func scanDirectory(path string) {
 	fmt.Println(path)              // printl current directory
 	files, err := os.ReadDir(path) // get a slice with the directory's contents.
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	for _, file := range files {
 		filePath := filepath.Join(path, file.Name())
 		if file.IsDir() {
-			err := scanDirectory(filePath)
-			if err != nil {
-				return err
-			}
+			scanDirectory(filePath)
+
 		} else {
 			fmt.Println(filePath)
 		}
 	}
-	return nil
+
 }
 
 func main() {
-	err := scanDirectory("directories")
-	if err != nil {
-		log.Fatal(err)
-	}
+	scanDirectory("directories")
+
 }
