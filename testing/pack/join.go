32f02f7b4989cb0cb520e3f02606180587b9ad41
Wakisa Mwabulambo <wakielambo@gmail.com>
2025-01-16 14:18:49 +0200

    Testing using testing package

--- testing/pack/join.go
+++ testing/pack/join.go
@@ -4,7 +4,7 @@ import "strings"
 
 func JoinWithCommas(phrases []string) string {
 	result := strings.Join(phrases[:len(phrases)-1], ", ") // Join every phrase except the last with commas
-	result += " and "                                      //Insert the word "and" before the last phrase
+	result += ", and "                                     //Insert the word "and" before the last phrase
 	result += phrases[len(phrases)-1]                      // add the last phrase
 	return result
 }
