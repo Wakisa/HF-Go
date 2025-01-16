ba488d13139b6834b78f4b9b5a7dff2aaf9295de
Wakisa Mwabulambo <wakielambo@gmail.com>
2025-01-16 14:58:04 +0200

    After updating the join.go to pass the test

--- testing/pack/join.go
+++ testing/pack/join.go
@@ -3,8 +3,12 @@ package pack
 import "strings"
 
 func JoinWithCommas(phrases []string) string {
-	result := strings.Join(phrases[:len(phrases)-1], ", ") // Join every phrase except the last with commas
-	result += ", and "                                     //Insert the word "and" before the last phrase
-	result += phrases[len(phrases)-1]                      // add the last phrase
-	return result
+	if len(phrases) == 2 {
+		return phrases[0] + " and " + phrases[1] // if the slice has just two items, just join them with "and"
+	} else {
+		result := strings.Join(phrases[:len(phrases)-1], ", ") // Join every phrase except the last with commas
+		result += ", and "                                     //Insert the word "and" before the last phrase
+		result += phrases[len(phrases)-1]                      // add the last phrase
+		return result
+	}
 }
