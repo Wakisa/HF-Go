389e214c81ff1aace73f10632501640f8778dbed
Wakisa Mwabulambo <wakielambo@gmail.com>
2025-01-16 14:47:25 +0200

    Testing using helper func to display error

--- testing/pack/join_test.go
+++ testing/pack/join_test.go
@@ -1,13 +1,16 @@
 package pack
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestTwoElements(t *testing.T) {
 	list := []string{"apple", "orange"}
 	want := "apple and orange"
 	got := JoinWithCommas(list)
 	if got != want {
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+		t.Error(errorString(list, got, want))
 	}
 
 }
@@ -17,7 +20,11 @@ func TestThreeElements(t *testing.T) {
 	want := "apple, orange, and pear"
 	got := JoinWithCommas(list)
 	if got != want {
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+		t.Error(errorString(list, got, want))
 	}
 
+}
+
+func errorString(list []string, got string, want string) string {
+	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
 }
