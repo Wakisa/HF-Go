eac488364c8e313ec1dd4b1cb1e26393814b1623
Wakisa Mwabulambo <wakielambo@gmail.com>
2025-01-22 15:53:47 +0200

    Responding differently for different resource paths

--- webApp/main.go
+++ webApp/main.go
@@ -5,17 +5,31 @@ import (
 	"net/http"
 )
 
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-
-	message := []byte("Hello, web!")
-	_, err := writer.Write(message) // Add "Hello, web" to the response
+func write(writer http.ResponseWriter, message string) {
+	_, err := writer.Write([]byte(message)) // convert the string to a slice of bytes
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func englishHandler(writer http.ResponseWriter, request *http.Request) {
+
+	write(writer, "Hello, Web!") // wrtites the string to the response.
+}
+
+func frenchHandler(writer http.ResponseWriter, request *http.Request) {
+
+	write(writer, "Salut, Web!") // wrtites the string to the response.
+}
+func hindiHandler(writer http.ResponseWriter, request *http.Request) {
+
+	write(writer, "Namaste, Web!") // wrtites the string to the response.
+}
+
 func main() {
-	http.HandleFunc("/hello", viewHandler)
+	http.HandleFunc("/hello", englishHandler)
+	http.HandleFunc("/salut", frenchHandler)
+	http.HandleFunc("/namaste", hindiHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
